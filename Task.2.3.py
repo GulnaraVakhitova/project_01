d8993410efb849cbfeddda3f7739d8ecf0d7733e
GulnaraVakhitova <133780152+GulnaraVakhitova@users.noreply.github.com>
2023-06-12 22:34:56 +0300

    Задача#7

---
+++ Task.2.3.py
@@ -0,0 +1,28 @@
+# Задача 2.3.
+
+# Напишите функцию, которая принимает цифры от 0 до 9 и возвращает значение прописью.
+# Например,
+# switch_it_up(1) -> 'One'
+# switch_it_up(3) -> 'Three'
+# switch_it_up(10000) -> None
+# Использовать условный оператор if-elif-else нельзя!
+
+def switch_it_up(number):
+    dict_num = {
+        0: 'Zero',
+        1: 'One',
+        2: 'Two',
+        3: 'Three',
+        4: 'Four',
+        5: 'Five',
+        6: 'Six',
+        7: 'Seven',
+        8: 'Eight',
+        9: 'Nine',
+        1000: 'None'
+    }
+
+    return dict_num.get(number)
+a = int(input('Введите число: '))
+b = switch_it_up(a)
+print(b)
