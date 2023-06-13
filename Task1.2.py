4a54b2d4f9cad71b4b42675b2aa999674c039f68
GulnaraVakhitova <133780152+GulnaraVakhitova@users.noreply.github.com>
2023-06-13 10:21:29 +0300

    Update Task1.2.py
    Исправленный и улучшенный вариант

--- Task1.2.py
+++ Task1.2.py
@@ -20,6 +20,7 @@
 # Пункт D.
 # Переведите минуты и секунды в формат времени. Используйте модуль datetime
 
+# Пункт А
 my_favorite_songs = [
     ['Waste a Moment', 3.03],
     ['New Salvation', 4.02],
@@ -32,18 +33,33 @@ my_favorite_songs = [
     ['In This World', 4.02],
 ]
 print(len(my_favorite_songs))
+sum_sound_time = 0
+sum_sound_time += my_favorite_songs[2][1]
+sum_sound_time += my_favorite_songs[4][1]
+sum_sound_time += my_favorite_songs[7][1]
+print('Три песни звучат', round(sum_sound_time, 2), 'минут')
 
-import random
-random.choice(my_favorite_songs)
-# print(random.choice(my_favorite_songs))
-a = random.choice(my_favorite_songs)
-b = random.choice(my_favorite_songs)
-c = random.choice(my_favorite_songs)
+# Пункт В
+my_favorite_songs_dict = {
+    'Waste a Moment': 3.03,
+    'New Salvation': 4.02,
+    'Staying\' Alive': 3.40,
+    'Out of Touch': 3.03,
+    'A Sorta Fairytale': 5.28,
+    'Easy': 4.15,
+    'Beautiful Day': 4.04,
+    'Nowhere to Run': 2.58,
+    'In This World': 4.02,
+}
 
-print(a)
-print(b)
-print(c)
+sum_sound_time = 0
+sum_sound_time += my_favorite_songs_dict['Waste a Moment']
+sum_sound_time += my_favorite_songs_dict['A Sorta Fairytale']
+sum_sound_time += my_favorite_songs_dict['In This World']
 
+print('Три песни звучат', round(sum_sound_time, 2), 'минут')
+
+# Пункт С
 my_favorite_songs = [
     ['Waste a Moment', 3.03],
     ['New Salvation', 4.02],
@@ -67,3 +83,30 @@ sum_my_favorite_songs += b [1]
 sum_my_favorite_songs += c [1]
 
 print('Три песни звучат', round(sum_my_favorite_songs, 2), 'минут')
+
+# Пункт D
+import random
+import datetime
+
+my_favorite_songs = [
+    ['Waste a Moment', 3.03],
+    ['New Salvation', 4.02],
+    ['Staying Alive', 3.40],
+    ['Out of Touch', 3.03],
+    ['A Sorta Fairytale', 5.28],
+    ['Easy', 4.15],
+    ['Beautiful Day', 4.04],
+    ['Nowhere to Run', 2.58],
+    ['In This World', 4.02],
+]
+
+# Выбираем 3 случайные песни
+random_songs = random.sample(my_favorite_songs, 3)
+
+# Вычисляем общее время звучания с помощью переменной x
+total_x = sum(song[1] for song in random_songs)
+
+# Преобразуем общее время звучания в объект timedelta
+x = datetime.timedelta(minutes=int(total_x), seconds=int((total_x % 1) * 60))
+
+print("Три песни", "звучат", str(x))
